6dda4461528953e112f05e9a2c1cd6ecbfa771a3
Wanming Lin <wanming.lin@intel.com>
2017-12-28 16:27:59 +0800

    Update debug log

--- tools/ci/before_install.sh
+++ tools/ci/before_install.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -e
 
+GIT_STATUS=$(git status)
+GIT_PARAM=$(git rev-parse HEAD)
+
 if [[ $(./wpt test-jobs --includes $JOB; echo $?) -eq 0 ]]; then
     export RUN_JOB=1
     git submodule update --init --recursive 1>&2
     export DISPLAY=:99.0
     sh -e /etc/init.d/xvfb start 1>&2
-    GIT_STATUS=$(git status)
-    GIT_PARAM=$(git rev-parse HEAD)
     # For uploading the manifest
     export WPT_MANIFEST_FILE=$HOME/meta/MANIFEST-$(git rev-parse HEAD).json
 else
