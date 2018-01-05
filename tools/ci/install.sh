7d09651e5657a1ceb45818aa5e8835fdf87c211c
Wanming Lin <wanming.lin@intel.com>
2018-01-05 16:32:55 +0800

    remove secure id

--- tools/ci/install.sh
+++ tools/ci/install.sh
@@ -3,12 +3,12 @@ set -ex
 
 SCRIPT_DIR=$(dirname $(readlink -f "$0"))
 WPT_ROOT=$(readlink -f $SCRIPT_DIR/../..)
-cd $WPT_ROOT
 
 export DISPLAY=:99.0
 sh -e /etc/init.d/xvfb start 1>&2
 # For uploading the manifest
 export WPT_MANIFEST_FILE=$HOME/meta/MANIFEST-$(git rev-parse HEAD).json
 
+cd $WPT_ROOT
 pip install -U setuptools
 pip install -U requests
