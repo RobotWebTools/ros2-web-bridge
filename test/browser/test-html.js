17cad662f0e0c8bbe5057e4cefab7bef7c6920a7
qiuzhong <zhongx.qiu@intel.com>
2018-04-08 16:49:19 +0800

    [UT] Update the browser test cases to pass the CI
    Changes:
    * Disable the test cases of `Topic.toStream()` API as it's not exposed
    for roslibjs
    * Add time to load each test page to let testharness.js complete the
    browser testing and then the puppeteer read the results.

--- test/browser/test-html.js
+++ test/browser/test-html.js
@@ -21,27 +21,31 @@ const child = require('child_process');
 const puppeteer = require('puppeteer');
 const jsdom = require('jsdom');
 const async = require('async');
+const Mocha = require('mocha');
+const htmlTests = require('./html_list.json').htmls;
 
-var bridgePath = path.join(`{__dirname}/../`, 'bin', 'rosbridge.js');
-var bridge = child.fork(bridgePath);
-var server = child.fork(`${__dirname}/server.js`, {cwd: `${__dirname}`});
-var htmlTests = require('./html_list.json').htmls;
+let bridgePath = path.join(`{__dirname}/../`, 'bin', 'rosbridge.js');
+let bridge = child.fork(bridgePath);
+let server = child.fork(`${__dirname}/server.js`, {cwd: `${__dirname}`});
 
-var testResults = [];
+let browserTestResults = [];
 async.each(htmlTests, async function (html, callback) {
-  const browser = await puppeteer.launch({args: ['--no-sandbox']});
-  const page = await browser.newPage();
+  let browser = await puppeteer.launch({args: ['--no-sandbox']});
+  let page = await browser.newPage();
   await page.setViewport({width: 1280, height: 960});
-  await page.goto('http://127.0.0.1:8080/' + html, {waitUntil: 'load'});
+  await page.goto('http://127.0.0.1:8080/' + html.suite, {waitUntil: 'load'});
+  await page.waitFor(html.waitingTime);
 
-  var results = await page.evaluate(() => {
-    testCasesResult = document.querySelector('#results').outerHTML;
-    return testCasesResult;
+  let results = await page.evaluate(() => {
+    return document.querySelector('#results').outerHTML;
   });
 
-  var dom = new jsdom.JSDOM(results, { contentType: 'text/html'});
+  let dom = new jsdom.JSDOM(results, { contentType: 'text/html'});
+  let testSuite = {};
+  testSuite['suite'] = html.suite;
+  testSuite['results'] = [];
   for (let i = 0; i < dom.window.document.querySelector('tbody').rows.length; i++) {
-    var testResult = {};
+    let testResult = {};
     testResult['caseId'] = dom.window.document.querySelector('tbody').rows[i].cells[1].textContent;
     testResult['result'] = dom.window.document.querySelector('tbody').rows[i].cells[0].textContent;
     testResult['message'] = dom.window.document.querySelector('tbody').rows[i].cells[2].textContent;
@@ -50,17 +54,41 @@ async.each(htmlTests, async function (html, callback) {
     testResult['purpose'] = '';
     testResult['type'] = 'auto';
     testResult['comment'] = '';
-    testResult['suite'] = html;
 
-    testResults.push(testResult);
+    testSuite['results'].push(testResult);
   }
 
+  browserTestResults.push(testSuite);
   await browser.close();
 }, function () {
-  // server.kill('SIGINT');
-  // bridge.kill('SIGINT');
-  console.log(testResults);
-  testResults.forEach((testResult) => {
-    assert.deepStrictEqual(testResult['result'], 'Pass');
-  });    
+  server.kill('SIGINT');
+  bridge.kill('SIGINT');
+
+  let testBrowser = "'use strict';\n";
+  testBrowser += "const assert = require('assert');\n\n";
+  testBrowser += "describe('Roslibjs API testing over ros2-web-bridge', function() {\n";
+  browserTestResults.forEach((thisSuite, index) => {
+    testBrowser += "  describe('" + thisSuite.suite + "', function() {\n";
+    thisSuite['results'].forEach((thisResult, index) => {
+      testBrowser += "   it('" + thisResult.caseId + "', function() {\n";
+      testBrowser += "     assert.strictEqual('" + thisResult.result
+                  + "', 'Pass', String.raw`"
+                  + thisResult.message + "`);\n";
+      testBrowser += "   });\n";
+    });
+    testBrowser += "  });\n\n";
+  });
+  testBrowser += "});\n";
+
+  fs.writeFile(path.join(`${__dirname}`, 'test-browser.js'), testBrowser, 'utf8',  (err) => {
+    if (err) throw err;
+
+    let mocha = new Mocha();
+    mocha.addFile(path.join(`${__dirname}`, 'test-browser.js'));
+    mocha.run(function(failures){
+      process.on('exit', function () {
+        process.exit(failures);
+      });
+    });
+  });
 });
